3776e9bb02f3e0160ac23f92c421a9e1a7fc239e
TaylanCann <taylancanh@gmail.com>
2022-02-20 20:33:49 +0300

    Add project files.

---
+++ DependencyInjectionUsingFalse/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DependencyInjectionUsingFalse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
