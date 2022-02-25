7fe2c9329f3a296d3e42623fd3b045e73b4cec0b
TaylanCann <taylancanh@gmail.com>
2022-02-25 18:22:23 +0300

    UsingFalse

---
+++ DependencyInjectionUsingFalse/CarMechanic.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyInjectionUsingFalse
+{
+    public class CarMechanic
+    {
+        public void changeWheels()
+        {
+            Nissan350Z nissan350Z = new Nissan350Z();
+            nissan350Z.changeWheels();
+
+            ToyotaSupra toyotaSupra = new ToyotaSupra();
+            toyotaSupra.changeWheels();
+        }
+    }
+
+    public class Car { }
+
+    public class Nissan350Z:Car 
+    {
+       public void changeWheels() 
+        {
+            Console.WriteLine("WheelsChanged");
+        }
+    }
+
+    public class ToyotaSupra : Car
+    {
+        public void changeWheels()
+        {
+            Console.WriteLine("WheelsChanged");
+        }
+    }
+}
